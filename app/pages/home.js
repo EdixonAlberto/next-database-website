1b6b26ffad280cb90e0bdcfb9ff7674b73d896d1
Daniel Solarte <danielsolartech@hotmail.com>
2020-04-24 13:19:37 -0500

    Add Home page

--- app/pages/home.js
+++ app/pages/home.js
@@ -9,9 +9,37 @@
 
 import React from 'react'
 import Header from '../components/header'
+import Code from '../components/code'
+import { Link } from 'react-router-dom'
 
 export default ({ docsHeader }) => {
-    return (<div className="next-database-home-page">
+    return <>
         <Header docsHeader={docsHeader} />
-    </div>)
+        <div className="next-database-home-page next-database-animation" animation-name="fadeIn">
+            <div className="home-information">
+                <div className="information-left">
+                    <h1 className="next-database-animation" animation-name="fadeInLeft">
+                        next-database
+                        <span className="next-database-animation" animation-name="fadeInLeft"
+                            style={{ animationDelay: '.25s' }}>
+                            v1.0.5
+                        </span>
+                    </h1>
+                    <p className="next-database-animation" animation-name="fadeInLeft" style={{ animationDelay: '.25s' }}>
+                        With next-database you can make queries to MySQL or PostgreSQL more easy
+                    </p>
+                    <div className="next-database-animation" animation-name="fadeInLeft" style={{ animationDelay: '.5s' }}>
+                        <Code lang="sh" text="$ npm i next-database" noMargin={true} />
+                    </div>
+                    <Link to="/docs/getting-started" className="next-database-animation" animation-name="fadeInDown"
+                        style={{ animationDelay: '1s' }}>
+                        Getting Started
+                    </Link>
+                </div>
+                <div className="information-right next-database-animation" animation-name="fadeInRight" animation-fast="">
+                    <div className="information-image"></div>
+                </div>
+            </div>
+        </div>
+    </>
 }

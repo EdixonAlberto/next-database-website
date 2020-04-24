5454fa45d905cd91fd4e840b08c21ecea1c8da7e
Daniel Solarte <danielsolartech@hotmail.com>
2020-04-23 21:30:03 -0500

    Add Left Join explain

--- app/pages/docs/leftJoin.js
+++ app/pages/docs/leftJoin.js
@@ -16,8 +16,15 @@ export default () => {
 
     return <>
         <h1>Left Join</h1>
+        <p>With this method you can get all rows from the left table, even if there are no matches in the right table</p>
+        <Code lang="javascript" text="selectQuery.leftJoin(table: string) : nextDatabase.Join" />
 
-        <DocsExtra asJoin={true} onJoin={true} usingJoin={true} />
+        <h2>Example</h2>
+        <Code lang="javascript" text="database.select('users').leftJoin('groups');" />
+        <p>It is equivalent to</p>
+        <Code lang="sql" text="SELECT * FROM 'users' LEFT JOIN 'groups';" />
+
+        <DocsExtra asJoin={true} onJoin={true} usingJoin={true} execute={true} />
 
         <DocsLinks back={{ title: "Full Join Query", link: "full-join" }}
             forward={{ title: "Right Join Query", link: "right-join" }} />
